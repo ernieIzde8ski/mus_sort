ea3d34ec76ab29621755f0658fcb3cbc668c0ab7
ernieIzde8ski <44341687+ernieIzde8ski@users.noreply.github.com>
2021-12-22 17:26:33 -0500

    Put repeated logic into dedicated commands

--- mus_sort.py
+++ mus_sort.py
@@ -3,13 +3,16 @@ from pathlib import Path
 from typing import Generator, Optional
 
 from tinytag import TinyTag
+accepted_files = ('.mp3', '.wav', '.flac', '.aiff', '.aifc', '.aif', '.afc', '.oga', '.ogg', '.opus', '.wma',
+                  '.m4b', '.m4a', '.mp4')
 
-accepted_files = tuple(("." + i) for i in ("mp3", "bit", "wav", "wave", "opus",
-                                           "flac", "asf", "wma", "mp4", "m4a", "m4b", "aiff", "aif", "aifc"))
+
+def is_valid_file(path: Path) -> bool:
+    return path.is_file() and path.suffix.lower() in accepted_files
 
 
 def is_valid_dir(path: Path) -> bool:
-    """Checks that a path is both a directory and is not a git directory"""
+    """Checks that a path is both a directory and not a git or pycache directory"""
     return path.is_dir() and path.name != ".git" and path.name != "__pycache__"
 
 
@@ -24,12 +27,13 @@ replacements = [(": ", " - "), ]
 
 
 def fix_new_path(name: str) -> str:
-    """Makes a path suitable for Windows"""
+    """Shortens a string & ensures it will not break as a Windows path name"""
     return textwrap.fill(name.strip(), width=50, placeholder="(...)", max_lines=1).replace(": ", " - ").replace(":", ";").replace("\"", "'").replace("\\", "").replace("/", "").replace("|", "")
 
 
 def fix_new_paths(*names: str) -> Generator[str, None, None]:
     """Shortened method to call fix_new_path on multiple items
+
     Returns the same length of items"""
     return (fix_new_path(name) for name in names)
 
@@ -40,19 +44,20 @@ class AlbumDirStats:
     artist: Optional[str]
     album: Optional[str]
 
-    def __init__(self, dir: Path) -> None:
+    def __init__(self, dir: Path, *,
+                 year: str = None, genre: str = None, artist: str = None, album: str = None) -> None:
         self.dir = Path(dir)
-        self.year = None
-        self.genre = None
-        self.artist = None
-        self.album = None
+        self.year = year
+        self.genre = genre
+        self.artist = artist
+        self.album = album
 
 
 def get_album_stats(dir: Path) -> AlbumDirStats:
     resp = AlbumDirStats(dir)
-    filepaths = [i for i in dir.iterdir() if (i.is_file() and i.suffix.lower() in accepted_files)]
+    paths = [path for path in dir.iterdir() if is_valid_file(path)]
 
-    for path in filepaths:
+    for path in paths:
         track = TinyTag.get(path)
 
         for attr in ("year", "genre", "album", "artist"):
@@ -75,8 +80,6 @@ def sort(dir: Path, root_dir: Path = None, *, errs: list[tuple[str, str]] = None
             sort(path, root_dir, errs=errs)
 
     if is_album_directory(dir):
-        if dir.name == ".git":
-            return
         stats = get_album_stats(dir)
 
         genre = stats.genre if (stats.genre and stats.genre != "Other") else "UNKNOWN_GENRE"
@@ -86,13 +89,11 @@ def sort(dir: Path, root_dir: Path = None, *, errs: list[tuple[str, str]] = None
             album = stats.year + ". " + album
 
         genre, artist, album = fix_new_paths(genre, artist, album)
-
         print(genre, artist, album)
+        
         target_dir = root_dir / genre / artist
         target_dir.mkdir(parents=True, exist_ok=True)
-        target_dir.resolve()
-
-        target_dir = target_dir / album
+        target_dir /= album
 
         try:
             dir.rename(target_dir)
@@ -101,18 +102,23 @@ def sort(dir: Path, root_dir: Path = None, *, errs: list[tuple[str, str]] = None
             if errs is not None:
                 errs.append((artist, album))
         except PermissionError as err:
+            # WinError 5 always occurs when an album is being listened to through MusicBee.
             if err.winerror == 5 and errs is not None:
-                print(f"Access is denied for album '{artist} - {album}'. You listening to that?")
+                print(f"Access is denied for album '{artist} - {album}'.")
                 errs.append((artist, album))
             else:
                 raise err
 
 
 def cleanup(dir: Path) -> None:
+    """Deletes all empty directories. 
+
+    This assumes that, like on Windows, non-empty directories cannot be deleted.
+    """
     for path in dir.iterdir():
-        if is_valid_dir(path) is False:
-            continue
-        cleanup(path)
+        if is_valid_dir(path):
+            cleanup(path)
+
     try:
         dir.rmdir()
     except:
@@ -120,22 +126,31 @@ def cleanup(dir: Path) -> None:
 
 
 def main() -> None:
-    path = Path(input("Path?  "))
+    """Asks for input before running the sort and cleanup functions.
+
+    If no path is given, assumes current path. If path given starts with './', set root directory to current path
+    and format from given path.
+    """
+    dirs = [str(path) for path in Path(".").iterdir() if is_valid_dir(path)]
+    print(f"Subdirectories here: {', '.join(dirs)}")
+
+    path = input("Path?  ")
+    root = Path(".").resolve() if path.startswith("./") else None
+    path = Path(path)
+
     if not is_valid_dir(path):
-        raise ValueError(f"Path {path.resolve()} is not a valid path" )
+        raise ValueError(f"Path {path.resolve()} is not a valid directory")
 
     errors: list[tuple[str, str]] = []
-    sort(path, errs=errors)
-    cleanup(Path(path))
+    sort(path, root, errs=errors)
     if len(errors) > 0:
         print("\n\n\nErrors occurred for the following albums:")
         for error in errors:
             print(*error)
+    cleanup(path)
 
 
 if __name__ == "__main__":
-    dirs = [str(path) for path in Path(".").iterdir() if is_valid_dir(path)]
-    print(f"Subdirectories here: {', '.join(dirs)}")
     try:
         main()
     except Exception as e:
