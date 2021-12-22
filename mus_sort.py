44561c0d7859a3fd2ffe0bc56a7e25bf574d358d
ernieIzde8ski <44341687+ernieIzde8ski@users.noreply.github.com>
2021-12-22 13:20:02 -0500

    Improve exception handling, overall program logic

--- mus_sort.py
+++ mus_sort.py
@@ -1,18 +1,16 @@
 import textwrap
 from pathlib import Path
-from typing import Literal, Optional, Union
+from typing import Generator, Optional
 
 from tinytag import TinyTag
 
-tuple(map(lambda i: "." + i, ("mp3", "bit", "wav", "wave", "opus", "flac", "asf", "wma", "mp4", "m4a", "m4b", "aiff", "aif", "aifc")))
 accepted_files = tuple(("." + i) for i in ("mp3", "bit", "wav", "wave", "opus",
                                            "flac", "asf", "wma", "mp4", "m4a", "m4b", "aiff", "aif", "aifc"))
 
 
-def fix_new_path(name: str) -> str:
-    """Makes a path suitable for Windows"""
-    return textwrap.fill(name.strip(), width=50, placeholder="(...)", max_lines=1).replace(
-        ": ", " - ").replace(":", ";").replace("\"", "'").replace("\\", "").replace("/", "").replace("|", "")
+def is_valid_dir(path: Path) -> bool:
+    """Checks that a path is both a directory and is not a git directory"""
+    return path.is_dir() and path.name != ".git" and path.name != "__pycache__"
 
 
 def is_album_directory(dir: Path) -> bool:
@@ -20,6 +18,20 @@ def is_album_directory(dir: Path) -> bool:
         if item.suffix.lower() in accepted_files:
             return True
     return False
+
+
+replacements = [(": ", " - "), ]
+
+
+def fix_new_path(name: str) -> str:
+    """Makes a path suitable for Windows"""
+    return textwrap.fill(name.strip(), width=50, placeholder="(...)", max_lines=1).replace(": ", " - ").replace(":", ";").replace("\"", "'").replace("\\", "").replace("/", "").replace("|", "")
+
+
+def fix_new_paths(*names: str) -> Generator[str, None, None]:
+    """Shortened method to call fix_new_path on multiple items
+    Returns the same length of items"""
+    return (fix_new_path(name) for name in names)
 
 
 class AlbumDirStats:
@@ -54,27 +66,26 @@ def get_album_stats(dir: Path) -> AlbumDirStats:
     return resp
 
 
-def sort(dir: Path, root_dir: Path = None, *, errs: list[str] = None) -> None:
+def sort(dir: Path, root_dir: Path = None, *, errs: list[tuple[str, str]] = None) -> None:
     if root_dir is None:
         root_dir = dir.resolve()
 
-    for item in dir.iterdir():
-        if item.is_dir():
-            sort(item, root_dir, errs=errs)
+    for path in dir.iterdir():
+        if is_valid_dir(path):
+            sort(path, root_dir, errs=errs)
+
     if is_album_directory(dir):
         if dir.name == ".git":
             return
         stats = get_album_stats(dir)
 
-        genre = stats.genre if (
-            stats.genre and stats.genre != "Other") else "UNKNOWN_GENRE"
+        genre = stats.genre if (stats.genre and stats.genre != "Other") else "UNKNOWN_GENRE"
         artist = stats.artist or "UNKNOWN_ARTIST"
         album = stats.album or "Singles"
-        if stats.year:
+        if stats.year is not None:
             album = stats.year + ". " + album
-        genre = fix_new_path(genre)
-        artist = fix_new_path(artist)
-        album = fix_new_path(album)
+
+        genre, artist, album = fix_new_paths(genre, artist, album)
 
         print(genre, artist, album)
         target_dir = root_dir / genre / artist
@@ -88,13 +99,20 @@ def sort(dir: Path, root_dir: Path = None, *, errs: list[str] = None) -> None:
         except FileExistsError as err:
             print(err)
             if errs is not None:
-                errs.append(album)
+                errs.append((artist, album))
+        except PermissionError as err:
+            if err.winerror == 5 and errs is not None:
+                print(f"Access is denied for album '{artist} - {album}'. You listening to that?")
+                errs.append((artist, album))
+            else:
+                raise err
 
 
 def cleanup(dir: Path) -> None:
-    if dir.name == ".git" or not dir.is_dir():
-        return
-    [cleanup(subdir) for subdir in dir.iterdir()]
+    for path in dir.iterdir():
+        if is_valid_dir(path) is False:
+            continue
+        cleanup(path)
     try:
         dir.rmdir()
     except:
@@ -103,17 +121,22 @@ def cleanup(dir: Path) -> None:
 
 def main() -> None:
     path = Path(input("Path?  "))
+    if not is_valid_dir(path):
+        raise ValueError(f"Path {path.resolve()} is not a valid path" )
 
-    errors = []
+    errors: list[tuple[str, str]] = []
     sort(path, errs=errors)
     cleanup(Path(path))
     if len(errors) > 0:
         print("\n\n\nErrors occurred for the following albums:")
         for error in errors:
-            print(error)
+            print(*error)
 
 
 if __name__ == "__main__":
-    dirs = [("./" + str(path)) for path in Path(".").iterdir() if (path.is_dir() and path.name != ".git")]
+    dirs = [str(path) for path in Path(".").iterdir() if is_valid_dir(path)]
     print(f"Subdirectories here: {', '.join(dirs)}")
-    main()
+    try:
+        main()
+    except Exception as e:
+        print(e.__class__.__name__ + ":", e)
