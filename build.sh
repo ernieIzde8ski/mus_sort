ac30bbf6b46a3474416d9110a6ae116e1202bf21
Ernest Izdebski <44341687+ernieIzde8ski@users.noreply.github.com>
2022-12-28 00:34:19 -0500

    remove extant build.sh script

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-python setup.py sdist bdist_wheel && {
-    read -r -p "Upload to PyPI? " prompt
-    if [[ $prompt == "Y" || $prompt == "y" || $prompt = "" ]]; then
-            twine upload dist/**
-    fi
-}
-rm -rf build/ && rm -rf dist/
